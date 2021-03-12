6db83bbd696924ac0acef2d47b8f9661a0e3b269
luyutian <1127071993@qq.com>
2021-03-12 17:26:37 +0800

    first

---
+++ 03/stack/index.js
@@ -0,0 +1,9 @@
+// // 栈 数据结构 ，array模拟栈
+// const stack = []
+// stack.push(1)
+// stack.push(2)
+// const item1 = stack.pop()
+// const item2 = stack.pop() 
+
+const str = 'gh'
+console.log(str[0])  //g
---
+++ 04/queue/index.js
@@ -0,0 +1,6 @@
+// 队列 数据结构  用数组模拟队列
+const queue = []
+queue.push(1)
+queue.push(2)
+const item1 = queue.shift()
+const item2 = queue.shift()
